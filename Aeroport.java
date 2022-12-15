67a570365385959e9179a9642242391fdf61f013
annevio <80254475+annevio@users.noreply.github.com>
2022-12-15 11:14:19 +0100

    Add files via upload

---
+++ Aeroport.java
@@ -0,0 +1,32 @@
+public class Aeroport {
+
+    // declaration des variables
+
+    String IATA;
+    String Name;
+    String country;
+    double latitude;
+    double longitude;
+
+    public Aeroport(String name, String country){
+
+    };
+
+    public getIATA(String IATA){
+
+    };
+
+    public getLatitude(double latitude){
+
+    };
+
+    public getLongitude()(double longitude){
+
+    };
+
+    // override
+    
+    public toString(){
+
+    };
+}
