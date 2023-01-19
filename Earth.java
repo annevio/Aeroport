db978fdccdda71b67aa8a087f13fab3843d0812a
annevio <80254475+annevio@users.noreply.github.com>
2023-01-19 12:05:37 +0100

    Add files via upload

--- Earth.java
+++ Earth.java
@@ -1,9 +1,14 @@
 // on importe la librairie javafx precedemment telechargee et ajoutee au projet
 
+import javafx.animation.AnimationTimer;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
+
 import java.util.ArrayList;
 
 // la class earth hérite de la classe group qui appartient à javafx
@@ -18,27 +23,53 @@ public class Earth extends Group {
     }
 
     public Earth() {
-        blueSphere = new ArrayList<Sphere>();
+        blueSphere = new ArrayList<Sphere>(); // on créé notre sphere bleue
         sph = new Sphere(300); // diametre de la terre egal a 300 pixels
+
+        // on définit la lumière appliquée au matériau (=image de la terre)
+        PhongMaterial skin = new PhongMaterial();
+        skin.setDiffuseMap(new Image("file:./data/earth_lights_4800.png"));
+        skin.setSelfIlluminationMap(new Image("file:./data/earth_lights_4800.png"));
+        sph.setMaterial(skin);
+
         this.getChildren().add(sph); // on rajoute la terre à l'ensemble du group
         this.getTransforms().add(ry);
-    }
+
+
+            AnimationTimer animationTimer = new AnimationTimer() {
+                @Override
+                public void handle(long l) {
+                    ry.setAxis(new Point3D(0,1,0));
+                    ry.setAngle(l/50000000);
+                }
+            };
+            animationTimer.start();
+        }
 
     public Sphere createSphere(Aeroport a){
 
-        double latitude=Math.toRadians(a.getLatitude());
+        double latitude=Math.toRadians(a.getLatitude()); // on passe des degrés aux radians
         double longitude=Math.toRadians(a.getLongitude());
 
-        Sphere coloredSphere = new Sphere(2); // instanciation de l'objet coloredSphere de type Sphere
+        Sphere aeroportSphere = new Sphere(2); // instanciation de l'objet coloredSphere de type Sphere
 
-        //   Translate tz= new Translate(300.*Math.cos(latitude)*Math.sin(longitude),-300*Math.sin(latitude),-300*Math.cos(latitude)*Math.cos(longitude));
-        Translate tz = new Translate(0,0,-300);
-        coloredSphere.getTransforms().add(tz);
-        Rotate rTheta = new Rotate (50,0,0,300,Rotate.X_AXIS);
-        Rotate rPhi = new Rotate (50,0,0,300,Rotate.Y_AXIS);
-        coloredSphere.getTransforms().add(rTheta);
-        coloredSphere.getTransforms().add(rPhi);
+        // o
+        Translate tz = new Translate();
+        double x = 300*Math.cos(latitude)*Math.sin(longitude);
+        double y = -300*Math.sin(latitude)-300*Math.cos(latitude);
+        double z = -300*Math.cos(latitude)*Math.cos(longitude);
+        tz.setX(x);
+        tz.setY(y);
+        tz.setZ(z);
+        aeroportSphere.getTransforms().add(tz);
+        // rotation sur x et y
+        Rotate rotatex = new Rotate (50,0,0,-300,Rotate.X_AXIS);
+        Rotate rotatey = new Rotate (50,0,0,-300,Rotate.Y_AXIS);
 
-        return coloredSphere;
+        aeroportSphere.getTransforms().add(tz);
+        aeroportSphere.getTransforms().add(rotatex);
+        aeroportSphere.getTransforms().add(rotatey);
+
+        return aeroportSphere;
     }
 }
--- Interface.java
+++ Interface.java
@@ -1,4 +1,4 @@
-/* structure java fx de base
+/* structure de base d'une interface en java
 
 public class Interface extends Application {
     @Override
@@ -13,5 +13,43 @@ public class Interface extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+}*/
+
+import javafx.application.Application;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+public class Interface extends Application {
+
+    Translate tz = new Translate();
+    private double mousePosX;
+    private double mousePosY;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        World w = new World("./data/airport-codes_no_comma.csv"); // on créé l'objet w de type World qui réfère à notre liste d'aéroports
+
+        primaryStage.setTitle("La terre est plate"); // nom de
+
+        Earth earth = new Earth();
+        Scene ihm = new Scene(earth, 800, 600,true); // création de la Scene (=fenetre) de type Scene
+
+        // Objet de type caméra pour avoir la 3D
+
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setTranslateZ(-1000);
+        camera.setNearClip(0.1); // distances auxquelles l'objet n'est plus affiché
+        camera.setFarClip(3000.0); // distances auxquelles l'objet n'est plus affiché
+        camera.setFieldOfView(35);
+        ihm.setCamera(camera);
+
+        primaryStage.setScene(ihm);
+        primaryStage.show(); // on affiche
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
- */
